a0a4b3b606249f7bd87a9895dcd6db7f54c2c90e
IgnacioLeivaP <73830057+IgnacioLeivaP@users.noreply.github.com>
2024-12-06 19:25:14 -0300

    Revert "trying to fix this mess part 2"
    This reverts commit 6f9cf8b70505af901f922caeeaba4320a91bd33d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
   }
 })
